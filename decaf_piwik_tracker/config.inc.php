7c3768ec354eef6d79ce6edd5c5ba268296f45bc
mrlee <sven@v26.org>
2010-08-10 14:36:23 +0000

    propset svn keywords

--- decaf_piwik_tracker/config.inc.php
+++ decaf_piwik_tracker/config.inc.php
@@ -10,20 +10,13 @@
 
 $mypage = "decaf_piwik_tracker";
 
-
-// just in case, we allow url_fopen wrapper
-// ini_set("allow_url_fopen", "On");
-
-
-
 $REX['ADDON']['rxid'][$mypage]    = "770";
 $REX['ADDON']['page'][$mypage]    = $mypage;
-$REX['ADDON']['version'][$mypage] = "0.1";
+$REX['ADDON']['version'][$mypage] = "1.0.0";
 $REX['ADDON']['author'][$mypage]  = "Sven Kesting &lt;sk@decaf.de&gt;, DECAF&deg; | www.decaf.de";
 $REX['ADDON']['perm'][$mypage]    = "decaf_piwik_tracker[]";
 $REX['PERM'][]                    = "decaf_piwik_tracker[]";
 $REX['PERM'][]                    = "decaf_piwik_tracker[config]";
-// $REX['PERM'][]                    = "decaf_piwik_tracker[widgets]";
 
 $REX['ADDON'][$mypage]['options']['color_background']     = '#eff9f9';
 $REX['ADDON'][$mypage]['options']['color_background_alt'] = '#dfe9e9';
@@ -31,9 +24,6 @@ $REX['ADDON'][$mypage]['options']['color_visits']         = '#14568a';
 $REX['ADDON'][$mypage]['options']['color_uniq_visitors']  = '#3c9ed0';
 $REX['ADDON'][$mypage]['options']['color_actions']        = '#5ab8ef';
 $REX['ADDON'][$mypage]['options']['color_text']           = '#000';
-
-// $REX['ADDON'][$mypage]['options']['show']                 = array('nb_visits');
-
 
 if ($REX['REDAXO'])
 {
@@ -55,19 +45,6 @@ if ($REX['REDAXO'])
 
       $settingsPage->setHref('index.php?page=decaf_piwik_tracker&subpage=settings');
     }
-    /*
-    if($REX_USER && ($REX_USER->isValueOf('rights','admin[]') || $REX_USER->isValueOf('rights','decaf_bbclonestats[widgets]') ))
-    {
-      // add menu buttons
-      $widgetsPage = new rex_be_page($I18N->msg('piwik_widgets'), array(
-          'page'=>'decaf_piwik_tracker',
-          'subpage'=>'widgets'
-        )
-      );
-
-      $widgetsPage->setHref('index.php?page=decaf_piwik_tracker&subpage=widgets');
-    }
-    */
 
     $statsPage = new rex_be_page($I18N->msg('piwik_ministats'), array(
         'page'=>'decaf_piwik_tracker',
@@ -82,10 +59,8 @@ if ($REX['REDAXO'])
   }
 }
 
-
 // include extension point only in frontend
 if (!$REX['REDAXO'])
 {
   require_once($REX['INCLUDE_PATH']."/addons/decaf_piwik_tracker/extensions/extension.decaf_piwik_tracker.inc.php");
 }
-
---
+++ decaf_piwik_tracker/config/_widgets.ini.php
@@ -0,0 +1,25 @@
+; <?php die('No Access');
+[0]
+api_period        = day                                       ; day, week, month or year
+api_date          = last14                                    ; lastX, previousX
+width             = 745                                       ; width in px
+columns           = nb_visits,nb_actions                      ; nb_uniq_visitors,nb_visits,nb_actions (No spaces!)
+
+; [1]
+; api_period        = week
+; api_date          = last6
+; width             = 745
+; columns           = nb_visits,nb_actions
+; 
+; [2]
+; ; widget_title      = This and last year
+; api_period        = year
+; api_date          = last2
+; width             = 362
+; columns           = nb_actions
+; 
+; [3]
+; api_period        = month
+; api_date          = last3
+; width             = 362
+; columns           = nb_visits
