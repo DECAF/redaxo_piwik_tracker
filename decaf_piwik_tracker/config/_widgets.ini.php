fb5e5d806596bf3f72efeb8f4ee6cae502e7086f
Dirk Schuerjohann <ds@decaf.de>
2010-12-22 14:48:34 +0000

    default widget config: add 4 months and 2 years

--- decaf_piwik_tracker/config/_widgets.ini.php
+++ decaf_piwik_tracker/config/_widgets.ini.php
@@ -5,21 +5,15 @@ api_date          = last14                                    ; lastX, previousX
 width             = 745                                       ; width in px
 columns           = nb_visits                                 ; nb_uniq_visitors,nb_visits,nb_actions (No spaces!)
 
-; [1]
-; api_period        = week
-; api_date          = last6
-; width             = 745
-; columns           = nb_visits,nb_actions
-; 
-; [2]
-; ; widget_title      = This and last year
-; api_period        = year
-; api_date          = last2
-; width             = 362
-; columns           = nb_actions
-; 
-; [3]
-; api_period        = month
-; api_date          = last3
-; width             = 362
-; columns           = nb_visits
+[1]
+api_period        = month
+api_date          = last4
+width             = 402
+columns           = nb_visits,nb_actions
+
+[2]
+; widget_title      = This and last year
+api_period        = year
+api_date          = last2
+width             = 322
+columns           = nb_visits,nb_actions
