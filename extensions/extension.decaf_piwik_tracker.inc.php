f6a5b0edf35bcb5ed4d67b4dc48857446e2d6f76
schuer <ds@decaf.de>
2012-12-10 00:41:27 +0100

    Some tracker enhancements: * do not track users logged into REDAXO * show debug msg
    Part of implementing #2

--- extensions/extension.decaf_piwik_tracker.inc.php
+++ extensions/extension.decaf_piwik_tracker.inc.php
@@ -21,35 +21,57 @@ function decaf_piwik_tracker($params)
     $content = $params['subject'];
 
     // Frontend
+    $debugMsg = $trackingSnippet = NULL;
     if ($piwik_config['piwik']['tracker_url'] && $piwik_config['piwik']['site_id'])
     {
+      // JavaScript tracking
       if ($piwik_config['piwik']['tracking_method'] == 'Javascript') 
       {
-        $js = "
-          <!-- Piwik -->
-          <script type=\"text/javascript\">
-          var pkBaseURL = \"".$piwik_config['piwik']['tracker_url']."\" + \"/\";
-          document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\"));
-          </script><script type=\"text/javascript\">
-          try {
-          var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", ".$piwik_config['piwik']['site_id'].");
-          piwikTracker.trackPageView();
-          piwikTracker.enableLinkTracking();
-          } catch( err ) {}
-          </script><noscript><p><img src=\"".$piwik_config['piwik']['tracker_url']."piwik.php?idsite=".$piwik_config['piwik']['site_id']."\" style=\"border:0\" alt=\"\" /></p></noscript>
-          <!-- End Piwik Tag -->
-        ";
-        $content = str_replace("</body>", $js."</body>", $content);
-        return $content;
+        if($_SESSION[$REX['INSTNAME']]['UID'])
+        {
+          $debugMsg = 'Piwik Tracker: JavaScript. Did not track REDAXO user.';
+        } else {
+          $debugMsg = 'Piwik Tracker: JavaScript.';
+          $trackingSnippet = "
+            <!-- Piwik -->
+            <script type=\"text/javascript\">
+            var pkBaseURL = \"".$piwik_config['piwik']['tracker_url']."\" + \"/\";
+            document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\"));
+            </script><script type=\"text/javascript\">
+            try {
+            var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", ".$piwik_config['piwik']['site_id'].");
+            piwikTracker.trackPageView();
+            piwikTracker.enableLinkTracking();
+            } catch( err ) {}
+            </script><noscript><p><img src=\"".$piwik_config['piwik']['tracker_url']."piwik.php?idsite=".$piwik_config['piwik']['site_id']."\" style=\"border:0\" alt=\"\" /></p></noscript>
+            <!-- End Piwik Tag -->
+          ";
+        }
       }
+
+      // PHP tracking
       if ($piwik_config['piwik']['tracking_method'] == 'PHP' && ini_get('allow_url_fopen')) 
       {
-        require_once($REX['INCLUDE_PATH']. '/addons/'.$mypage.'/classes/PiwikTracker.php');
-        PiwikTracker::$URL = $piwik_config['piwik']['tracker_url'];
-        $piwikTracker = new PiwikTracker(  $idSite = $piwik_config['piwik']['site_id']);
-        preg_match('/<title>(.*)<\/title>/U', $content, $hits);
-        $piwikTracker->doTrackPageView($hits[1]);
+        if($_SESSION[$REX['INSTNAME']]['UID'])
+        {
+          $debugMsg = 'Piwik Tracker: PHP. Did not track REDAXO user.';
+        } else {
+          $debugMsg = 'Piwik Tracker: PHP.';
+          require_once($REX['INCLUDE_PATH']. '/addons/'.$mypage.'/classes/PiwikTracker.php');
+          PiwikTracker::$URL = $piwik_config['piwik']['tracker_url'];
+          $piwikTracker = new PiwikTracker(  $idSite = $piwik_config['piwik']['site_id']);
+          preg_match('/<title>(.*)<\/title>/U', $content, $hits);
+          if ($piwikTracker->doTrackPageView($hits[1]) != true)
+          {
+            $debugMsg .= ' FAILURE.';
+          } else {
+            $debugMsg .= ' SUCCESS.';
+          }
+        }
       }
+
+      $content = str_replace("</body>", $trackingSnippet.PHP_EOL."<!-- ".$debugMsg." -->".PHP_EOL."</body>", $content);
+      return $content;
     }
   }
 }
